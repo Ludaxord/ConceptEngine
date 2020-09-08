93e8d06a7b6aa2b2c7620983762f1b9ab0eff513
Konrad Uciechowski <konrad.uciechowski@gmail.com>
2020-09-08 13:55:52 +0200

    init mac os metal engine and changes in ignore file

---
+++ api/platform/apple/mac/metal/ConceptEngine/ConceptEngine/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ConceptEngine
+//
+//  Created by Konrad Uciechowski on 08/09/2020.
+//  Copyright © 2020 Ldx Studio. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
