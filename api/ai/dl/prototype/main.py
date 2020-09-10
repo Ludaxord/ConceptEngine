392676d838c508d023bebc8b355f522a849d3233
Konrad Uciechowski <konrad.uciechowski@gmail.com>
2020-09-10 12:31:54 +0200

    create ai directory

---
+++ api/ai/dl/prototype/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Concept Engine Deep Learning AI Prototype...")
+
+
+if __name__ == "__main__":
+    main()
---
+++ api/ai/ml/prototype/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Concept Engine Machine Learning AI Prototype...")
+
+
+if __name__ == "__main__":
+    main()
