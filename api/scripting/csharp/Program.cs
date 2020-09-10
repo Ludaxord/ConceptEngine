308973b5a9494cc434514f7bb84dbca76e5269d2
Konrad Uciechowski <konrad.uciechowski@gmail.com>
2020-09-10 11:48:39 +0200

    main of scripting

---
+++ api/scripting/csharp/Program.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace ConceptEngine.Scripting {
+    class Program {
+        static void Main(string[] args){
+            Console.WriteLine("Welcome to ConceptEngine...");
+        }
+    }
+}
---
+++ api/scripting/python/main.py
@@ -0,0 +1,5 @@
+def main():
+    print("Welcome to ConceptEngine...")
+
+if __name__ == "__main__":
+    main()
