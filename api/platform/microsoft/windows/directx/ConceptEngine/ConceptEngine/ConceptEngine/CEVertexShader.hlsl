3f46b2b6a227d96020f30fc019e908ebef0b2b97
Konrad Uciechowski <konrad.uciechowski@gmail.com>
2020-11-27 20:55:45 +0100

    add usage of ConstantBuffer in HLSL and make connected triangles to rotate

--- api/platform/microsoft/windows/directx/ConceptEngine/ConceptEngine/ConceptEngine/CEVertexShader.hlsl
+++ api/platform/microsoft/windows/directx/ConceptEngine/ConceptEngine/ConceptEngine/CEVertexShader.hlsl
@@ -3,10 +3,13 @@ struct CEVertexOut {
 	float4 pos: SV_POSITION;
 };
 
+cbuffer CEConstantBuffer {
+	matrix transform;
+}
 
 CEVertexOut main(float2 pos : POSITION, float4 color : COLOR) {
 	CEVertexOut vso;
-	vso.pos = float4(pos.x, pos.y, 0.0f, 1.0f);
+	vso.pos = mul(float4(pos.x, pos.y, 0.0f, 1.0f), transform);
 	vso.color = color;
 	return vso;
 }
