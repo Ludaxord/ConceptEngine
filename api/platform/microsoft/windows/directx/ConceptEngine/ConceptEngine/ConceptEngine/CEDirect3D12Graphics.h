0dd8f3f52bfdd6788f823155c719cc86872a8cd4
Konrad Uciechowski <konrad.uciechowski@gmail.com>
2020-12-02 19:13:13 +0100

    add graphics api variables responsible for direct3d 12 device

--- api/platform/microsoft/windows/directx/ConceptEngine/ConceptEngine/ConceptEngine/CEDirect3D12Graphics.h
+++ api/platform/microsoft/windows/directx/ConceptEngine/ConceptEngine/ConceptEngine/CEDirect3D12Graphics.h
@@ -1,5 +1,11 @@
 #pragma once
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <D3Dcompiler.h>
+#include <DirectXMath.h>
 #include "CEGraphics.h"
+
+using namespace DirectX;
 
 class CEDirect3D12Graphics : public CEGraphics {
 
@@ -12,4 +18,27 @@ public:
 	void ClearBuffer(float red, float green, float blue, float alpha) noexcept override;
 	void DrawDefaultFigure(float angle, float windowWidth, float windowHeight, float x, float y, float z,
 	                       CEDefaultFigureTypes figureTypes) override;
+
+private:
+	static const UINT FrameCount = 2;
+protected:
+	bool useWarpDevice;
+
+	Microsoft::WRL::ComPtr<IDXGISwapChain3> pSwap;
+	Microsoft::WRL::ComPtr<ID3D12Device> pDevice;
+	Microsoft::WRL::ComPtr<ID3D12Resource> pTargets[FrameCount];
+	Microsoft::WRL::ComPtr<ID3D12CommandAllocator> m_commandAllocator;
+	Microsoft::WRL::ComPtr<ID3D12CommandQueue> m_commandQueue;
+	Microsoft::WRL::ComPtr<ID3D12RootSignature> m_rootSignature;
+	Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> m_rtvHeap;
+	Microsoft::WRL::ComPtr<ID3D12PipelineState> m_pipelineState;
+	Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> m_commandList;
+	UINT m_rtvDescriptorSize;
+	Microsoft::WRL::ComPtr<ID3D12Resource> m_vertexBuffer;
+	D3D12_VERTEX_BUFFER_VIEW m_vertexBufferView;
+
+	UINT m_frameIndex;
+	HANDLE m_fenceEvent;
+	Microsoft::WRL::ComPtr<ID3D12Fence> m_fence;
+	UINT64 m_fenceValue;
 };
