f87f80192b618bc89514e82ad57b720e55010e6b
Konrad Uciechowski <konrad.uciechowski@gmail.com>
2020-12-21 18:26:26 +0100

    prepare command queue functions

--- api/platform/microsoft/windows/ConceptEngine/ConceptEngine/ConceptEngineFramework/CED3D12CommandQueue.cpp
+++ api/platform/microsoft/windows/ConceptEngine/ConceptEngine/ConceptEngineFramework/CED3D12CommandQueue.cpp
@@ -1,5 +1,5 @@
 #include "CED3D12CommandQueue.h"
 
 
-CED3D12CommandQueue::CED3D12CommandQueue(wrl::ComPtr<ID3D12Device2> device, D3D12_COMMAND_LIST_TYPE type) {
+CED3D12CommandQueue::CED3D12CommandQueue(wrl::ComPtr<ID3D12Device> device, D3D12_COMMAND_LIST_TYPE type) {
 }
--- api/platform/microsoft/windows/ConceptEngine/ConceptEngine/ConceptEngineFramework/CED3D12CommandQueue.h
+++ api/platform/microsoft/windows/ConceptEngine/ConceptEngine/ConceptEngineFramework/CED3D12CommandQueue.h
@@ -7,12 +7,29 @@ namespace wrl = Microsoft::WRL;
 
 class CED3D12CommandQueue {
 public:
-	CED3D12CommandQueue(wrl::ComPtr<ID3D12Device2> device, D3D12_COMMAND_LIST_TYPE type);
+	CED3D12CommandQueue(wrl::ComPtr<ID3D12Device> device, D3D12_COMMAND_LIST_TYPE type);
 	virtual ~CED3D12CommandQueue();
+	// Get an available command list from the command queue.
+	wrl::ComPtr<ID3D12GraphicsCommandList> GetCommandList();
 
+	// Execute a command list.
+	// Returns the fence value to wait for for this command list.
+	uint64_t ExecuteCommandList(wrl::ComPtr<ID3D12GraphicsCommandList> commandList);
+
+	bool IsFenceComplete(uint64_t fenceValue);
+	void WaitForPreviousFrame(uint64_t fenceValue);
+protected:
+	wrl::ComPtr<ID3D12CommandAllocator> CreateCommandAllocator();
+	wrl::ComPtr<ID3D12GraphicsCommandList> CreateCommandList(wrl::ComPtr<ID3D12CommandAllocator> allocator);
+
+
+	wrl::ComPtr<ID3D12CommandQueue> GetD3D12CommandQueue() const;
 private:
 	struct CECommandAllocatorEntry {
 		uint64_t fenceValue;
 		wrl::ComPtr<ID3D12CommandAllocator> commandAllocator;
 	};
+
+	using CommandAllocatorQueue = std::queue<CECommandAllocatorEntry>;
+	using CommandListQueue = std::queue<wrl::ComPtr<ID3D12GraphicsCommandList>>;
 };
