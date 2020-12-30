febedfdb534616408e3ad323d0dd85e3c4a22127
Konrad Uciechowski <konrad.uciechowski@gmail.com>
2020-12-30 13:30:37 +0100

    add logger library to source, change git ignore file and add library to git modules:

--- api/platform/microsoft/windows/ConceptEngine/ConceptEngine/ConceptEngineFramework/ConceptEngine.h
+++ api/platform/microsoft/windows/ConceptEngine/ConceptEngine/ConceptEngineFramework/ConceptEngine.h
@@ -1,25 +1,24 @@
 #pragma once
 #include <Windows.h>
 
-
 #include "CEOSTools.h"
 #include "CETimer.h"
 #include "CEWindow.h"
+
+// using Logger = std::shared_ptr<spdlog::logger>;
 
 class ConceptEngine {
 public:
 	ConceptEngine();
 	ConceptEngine(int width, int height, const char* name);
 	ConceptEngine(int width, int height, const char* name, CEOSTools::CEGraphicsApiTypes graphicsApiType);
-	//TODO: Add structure with object that need to appear on the screen on Engine Run;
-	//TODO: -> Run() will call LoadContent and init objects that are in passed structure as parameter.
-	//TODO: For now objects are created in LoadContent function of every Graphics API
 	int Run();
 	void MakeFrame();
 private:
 	CEOSTools::CEGraphicsApiTypes apiType_;
 	CEWindow window_;
 	CETimer timer_;
+	// Logger logger_;
 
 private:
 	int g_frameIndx;
