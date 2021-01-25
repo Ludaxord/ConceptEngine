a665398538c9f15db8e45f2c25697a4806c5b5b0
Konrad Uciechowski <konrad.uciechowski@gmail.com>
2021-01-26 00:03:21 +0100

    create

--- api/platform/microsoft/windows/ConceptEngine/ConceptEngine/ConceptEngineEditor/WinMain.cpp
+++ api/platform/microsoft/windows/ConceptEngine/ConceptEngine/ConceptEngineEditor/WinMain.cpp
@@ -5,15 +5,7 @@ using namespace Concept;
 
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
 
-	// try {
-		ConceptEngine engine = ConceptEngine(hInstance);
-		return engine.Run();
-	// }
-	// catch (const std::exception& e) {
-	// 	MessageBox(nullptr, CETools::ConvertCharArrayToLPCWSTR(e.what()), L"Exception",MB_OK | MB_ICONEXCLAMATION);
-	// }
-	// catch (...) {
-	// 	MessageBox(nullptr, L"No error details", L"Exception",MB_OK | MB_ICONEXCLAMATION);
-	// }
-	return -1;
+	ConceptEngine engine = ConceptEngine(hInstance);
+	return engine.Run();
+
 }
