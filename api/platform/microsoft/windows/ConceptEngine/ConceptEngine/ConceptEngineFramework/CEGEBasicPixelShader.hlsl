de9281ee41c7952c5ed9a5adad7173d76f608d75
Konrad Uciechowski <konrad.uciechowski@gmail.com>
2021-01-27 23:49:36 +0100

    check change if place only setted color in pixel shader output

--- api/platform/microsoft/windows/ConceptEngine/ConceptEngine/ConceptEngineFramework/CEGEBasicPixelShader.hlsl
+++ api/platform/microsoft/windows/ConceptEngine/ConceptEngine/ConceptEngineFramework/CEGEBasicPixelShader.hlsl
@@ -7,5 +7,6 @@ float4 main(PixelShaderInput IN) : SV_TARGET {
 	/*
 	 * Return gamma corrected color
 	 */
-	return pow(abs(IN.Color), 1.0f / 2.2f);
+	return pow(abs(IN.Color), 1.0f / 2.0f);
+	// return IN.Color;
 }
