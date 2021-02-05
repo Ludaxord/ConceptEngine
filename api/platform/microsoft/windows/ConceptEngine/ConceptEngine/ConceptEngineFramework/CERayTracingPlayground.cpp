ae71b4c1261fba09082e9ab9dada2d0ec3bf5b2a
Konrad Uciechowski <konrad.uciechowski@gmail.com>
2021-02-05 20:17:42 +0100

    remove Nvidia library and non finished RTX playground

--- api/platform/microsoft/windows/ConceptEngine/ConceptEngine/ConceptEngineFramework/CERayTracingPlayground.cpp
+++ api/platform/microsoft/windows/ConceptEngine/ConceptEngine/ConceptEngineFramework/CERayTracingPlayground.cpp
@@ -1,1 +1,36 @@
 #include "CERayTracingPlayground.h"
+
+Concept::GameEngine::Playground::CERayTracingPlayground::CERayTracingPlayground(const std::wstring& name,
+	uint32_t width, uint32_t height, bool vSync): CEPlayground(name, width, height, vSync) {
+}
+
+bool Concept::GameEngine::Playground::CERayTracingPlayground::LoadContent() {
+	return true;
+}
+
+void Concept::GameEngine::Playground::CERayTracingPlayground::UnloadContent() {
+}
+
+void Concept::GameEngine::Playground::CERayTracingPlayground::OnUpdate(UpdateEventArgs& e) {
+}
+
+void Concept::GameEngine::Playground::CERayTracingPlayground::OnRender() {
+}
+
+void Concept::GameEngine::Playground::CERayTracingPlayground::OnKeyPressed(KeyEventArgs& e) {
+}
+
+void Concept::GameEngine::Playground::CERayTracingPlayground::OnKeyReleased(KeyEventArgs& e) {
+}
+
+void Concept::GameEngine::Playground::CERayTracingPlayground::OnMouseMoved(MouseMotionEventArgs& e) {
+}
+
+void Concept::GameEngine::Playground::CERayTracingPlayground::OnMouseWheel(MouseWheelEventArgs& e) {
+}
+
+void Concept::GameEngine::Playground::CERayTracingPlayground::OnResize(ResizeEventArgs& e) {
+}
+
+void Concept::GameEngine::Playground::CERayTracingPlayground::OnDPIScaleChanged(DPIScaleEventArgs& e) {
+}
--- api/platform/microsoft/windows/ConceptEngine/ConceptEngine/ConceptEngineFramework/CERayTracingPlayground.h
+++ api/platform/microsoft/windows/ConceptEngine/ConceptEngine/ConceptEngineFramework/CERayTracingPlayground.h
@@ -16,6 +16,20 @@ namespace Concept {
 
 	namespace GameEngine::Playground {
 		class CERayTracingPlayground : public CEPlayground {
+		public:
+			CERayTracingPlayground(const std::wstring& name, uint32_t width, uint32_t height, bool vSync = false);
+
+			bool LoadContent() override;
+			void UnloadContent() override;
+		protected:
+			void OnUpdate(UpdateEventArgs& e) override;
+			void OnRender() override;
+			void OnKeyPressed(KeyEventArgs& e) override;
+			void OnKeyReleased(KeyEventArgs& e) override;
+			void OnMouseMoved(MouseMotionEventArgs& e) override;
+			void OnMouseWheel(MouseWheelEventArgs& e) override;
+			void OnResize(ResizeEventArgs& e) override;
+			void OnDPIScaleChanged(DPIScaleEventArgs& e) override;
 		};
 	}
 }
