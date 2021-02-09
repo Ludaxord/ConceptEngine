8bf72013d3f6064b15e4e870d4037581ac8038d4
Konrad Uciechowski <konrad.uciechowski@gmail.com>
2021-02-09 22:36:33 +0100

    create Ray Tracing Shader

---
+++ api/platform/microsoft/windows/ConceptEngine/ConceptEngine/ConceptEngineFramework/CEGERayTracingVertexShader.hlsl
@@ -0,0 +1,41 @@
+void rayGen() {
+
+}
+
+void miss() {
+
+}
+
+//TODO: Change hardcoded objects in shaders to dynamic
+
+void triangleChs() {
+
+}
+
+void planeChs() {
+
+}
+
+void triangleHitGroup() {
+
+}
+
+void planeHitGroup() {
+
+}
+
+void shadowChs() {
+
+}
+
+void shadowMiss() {
+
+}
+
+void shadowHitGroup() {
+
+}
+
+float4 main(float4 pos : POSITION) : SV_POSITION {
+	return pos;
+}
