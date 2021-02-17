0249ea9b45a7c33e1b1fdeaac39a68a59128c54c
Konrad Uciechowski <konrad.uciechowski@gmail.com>
2021-02-17 21:24:08 +0100

    create project as library to check configuration, tomorrow turn it into framework

--- api/platform/microsoft/windows/ConceptEngine/ConceptEngine/ConceptEngineTutorialRunner/WinMain.cpp
+++ api/platform/microsoft/windows/ConceptEngine/ConceptEngine/ConceptEngineTutorialRunner/WinMain.cpp
@@ -1,10 +1,11 @@
 #include <Windows.h>
 
 
-#include "../DirectXBoxTutorial/DirectXBoxTutorial.h"
+#include "../RTHelloWorld/D3D12RaytracingHelloWorld.h"
+#include "../RTHelloWorld/Win32Application.h"
+
 
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-	const std::shared_ptr<DirectXBoxTutorial> tutorial = std::make_shared<DirectXBoxTutorial>(1920, 1080);
-
-	return ConceptEngineRunner::Run(tutorial, hInstance, nCmdShow);
+    D3D12RaytracingHelloWorld sample(1280, 720, L"D3D12 Raytracing - Hello World");
+    return Win32Application::Run(&sample, hInstance, nCmdShow);
 }
