9fa507305a3f9205a632d7a2a06bb46a862eeb2d
Konrad Uciechowski <konrad.uciechowski@gmail.com>
2021-02-24 20:47:05 +0100

    add comments to object that need to be created

--- ConceptEngine/ConceptEngine/ConceptEngine/Playgrounds/DirectX12/Samples/Landscape/CEDX12LandscapePlayground.cpp
+++ ConceptEngine/ConceptEngine/ConceptEngine/Playgrounds/DirectX12/Samples/Landscape/CEDX12LandscapePlayground.cpp
@@ -1,34 +1,66 @@
 #include "CEDX12LandscapePlayground.h"
 
+#include "../../../../../ConceptEngineFramework/Graphics/DirectX12/CEDX12Manager.h"
+
 using namespace ConceptEngine::Playgrounds::DirectX12;
 using namespace ConceptEngineFramework::Game;
 
 void CEDX12LandscapePlayground::Create() {
+	CEDX12Playground::Create();
+
+	//Reset command list to prepare for initialization commands
+	m_dx12manager->ResetCommandList();
+
+	//Create basic waves
+
+	//Create Root Signature
+
+	//Create Shaders and input layout
+
+	//Create Landscape geometry
+
+	//Create geometry buffers
+
+	//Create Render items
+
+	//Create Frame resources
+
+	//Create Pipeline State Objects
 }
 
 void CEDX12LandscapePlayground::Update(const CETimer& gt) {
+	CEDX12Playground::Update(gt);
+
 }
 
 void CEDX12LandscapePlayground::Render(const CETimer& gt) {
+	CEDX12Playground::Render(gt);
 }
 
 void CEDX12LandscapePlayground::Resize() {
+	CEDX12Playground::Resize();
 }
 
 void CEDX12LandscapePlayground::OnMouseDown(KeyCode key, int x, int y) {
+	CEDX12Playground::OnMouseDown(key, x, y);
 }
 
 void CEDX12LandscapePlayground::OnMouseUp(KeyCode key, int x, int y) {
+	CEDX12Playground::OnMouseUp(key, x, y);
 }
 
 void CEDX12LandscapePlayground::OnMouseMove(KeyCode key, int x, int y) {
+	CEDX12Playground::OnMouseMove(key, x, y);
 }
 
 void CEDX12LandscapePlayground::OnKeyUp(KeyCode key, char keyChar) {
+	CEDX12Playground::OnKeyUp(key, keyChar);
 }
 
 void CEDX12LandscapePlayground::OnKeyDown(KeyCode key, char keyChar) {
+	CEDX12Playground::OnKeyDown(key, keyChar);
 }
 
 void CEDX12LandscapePlayground::OnMouseWheel(KeyCode key, float wheelDelta, int x, int y) {
+	CEDX12Playground::OnMouseWheel(key, wheelDelta, x, y);
 }
--- ConceptEngine/ConceptEngine/ConceptEngine/WinMain.cpp
+++ ConceptEngine/ConceptEngine/ConceptEngine/WinMain.cpp
@@ -1,8 +1,11 @@
 #include <memory>
 #include <Windows.h>
 #include "../ConceptEngineFramework/CEFramework.h"
+
+//Samples imports
 #include "Playgrounds/DirectX12/Samples/Box/CEDX12BoxPlayground.h"
 #include "Playgrounds/DirectX12/Samples/InitDirect3D/CEDX12InitDirect3DPlayground.h"
+#include "Playgrounds/DirectX12/Samples/Landscape/CEDX12LandscapePlayground.h"
 
 using namespace ConceptEngineFramework;
 using namespace ConceptEngine::Playgrounds::DirectX12;
@@ -10,7 +13,7 @@ using namespace ConceptEngine::Playgrounds::DirectX12;
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
 	// Graphics::CEPlayground* playground = new CEDX12InitDirect3DPlayground();
 	// Graphics::CEPlayground* playground = new CEDX12BoxPlayground();
-	Graphics::CEPlayground* playground = new CEDX12BoxPlayground();
+	Graphics::CEPlayground* playground = new CEDX12LandscapePlayground();
 	const auto framework = std::make_shared<CEFramework>(L"ConceptEngine",
 	                                                     hInstance,
 	                                                     lpCmdLine,
