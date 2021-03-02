01f30fba22d5ae9b3ea79d3b939db7a816671395
Konrad Uciechowski <konrad.uciechowski@gmail.com>
2021-03-02 21:43:38 +0100

    modify vertex shader, todo: create structure for lit vertex Out implement Normal and add constant buffer with view projection

--- ConceptEngine/ConceptEngine/ConceptEngineFramework/Graphics/DirectX12/Resources/Shaders/CELitVertexShader.hlsl
+++ ConceptEngine/ConceptEngine/ConceptEngineFramework/Graphics/DirectX12/Resources/Shaders/CELitVertexShader.hlsl
@@ -1,4 +1,20 @@
-float4 VS( float4 pos : POSITION ) : SV_POSITION
-{
-	return pos;
+#include "VertexHeader.h"
+
+cbuffer cbPerObject : register(b0) {
+float4x4 gWorld;
+}
+
+VertexOut VS(VertexIn vIn) {
+	VertexOut vOut = (VertexOut)0.0f;
+
+	//Transform to world space
+	float4 posW = mul(float4(vIn.PosL, 1.0f), gWorld);
+
+	//Assumes nonuniform scaling; otherwise, need to use inverse-transpose of world matrix.
+	// vOut.NormalW = mul(vIn.NormalL, (float3x3)gWorld);
+
+	//transform to homogeneous clip space
+	// vOut.PosH = mul(posW, gViewProj);
+
+	return vOut;
 }
