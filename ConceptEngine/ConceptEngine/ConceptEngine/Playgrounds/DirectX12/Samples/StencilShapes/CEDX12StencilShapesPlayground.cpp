8dde492fd8b92c5b2e5731758710eefede86874c
Konrad Uciechowski <konrad.uciechowski@gmail.com>
2021-03-05 20:12:15 +0100

    add new playground

--- ConceptEngine/ConceptEngine/ConceptEngine/Playgrounds/DirectX12/Samples/StencilShapes/CEDX12StencilShapesPlayground.cpp
+++ ConceptEngine/ConceptEngine/ConceptEngine/Playgrounds/DirectX12/Samples/StencilShapes/CEDX12StencilShapesPlayground.cpp
@@ -570,14 +570,24 @@ void CEDX12StencilShapesPlayground::UpdateMainPassCB(const CETimer& gt) {
 	mMainPassCB.FarZ = 1000.0f;
 	mMainPassCB.TotalTime = gt.TotalTime();
 	mMainPassCB.DeltaTime = gt.DeltaTime();
-	mMainPassCB.AmbientLight = {0.25f, 0.25f, 0.35f, 1.0f};
-	mMainPassCB.Lights[0].Direction = {0.57735f, -0.57735f, 0.57735f};
-	mMainPassCB.Lights[0].Strength = {0.6f, 0.6f, 0.6f};
-	mMainPassCB.Lights[1].Direction = {-0.57735f, -0.57735f, 0.57735f};
-	mMainPassCB.Lights[1].Strength = {0.3f, 0.3f, 0.3f};
-	mMainPassCB.Lights[2].Direction = {0.0f, -0.707f, -0.707f};
-	mMainPassCB.Lights[2].Strength = {0.15f, 0.15f, 0.15f};
 
+	mMainPassCB.AmbientLight = { 0.25f, 0.25f, 0.35f, 1.0f };
+	// mMainPassCB.Lights[0].Direction = {0.57735f, -0.57735f, 0.57735f};
+	// mMainPassCB.Lights[0].Strength = {0.9f, 0.9f, 0.9f};
+	// mMainPassCB.Lights[1].Direction = {-0.57735f, -0.57735f, 0.57735f};
+	// mMainPassCB.Lights[1].Strength = {0.5f, 0.5f, 0.5f};
+	// mMainPassCB.Lights[2].Direction = {0.0f, -0.707f, -0.707f};
+	// mMainPassCB.Lights[2].Strength = {0.2f, 0.2f, 0.2f};
+
+	XMVECTOR lightDirection = -m_dx12manager->SphericalToCartesian(1.0f, mSunTheta, mSunPhi);
+
+	mMainPassCB.Lights[0].Strength = { 0.6f, 0.6f, 0.6f };
+	XMStoreFloat3(&mMainPassCB.Lights[0].Direction, lightDirection);
+	mMainPassCB.Lights[1].Strength = { 0.3f, 0.3f, 0.3f };
+	mMainPassCB.Lights[1].Direction = {-0.57735f, -0.57735f, 0.57735f};
+	mMainPassCB.Lights[2].Strength = { 0.15f, 0.15f, 0.15f };
+	mMainPassCB.Lights[2].Direction = {0.0f, -0.707f, -0.707f};
+	
 	// Main pass stored in index 2
 	auto currPassCB = mCurrFrameResource->PassCB.get();
 	currPassCB->CopyData(0, mMainPassCB);
---
+++ ConceptEngine/ConceptEngine/ConceptEngine/Playgrounds/DirectX12/Samples/TreesWaves/CEDX12TreesWavesPlayground.h
@@ -0,0 +1,82 @@
+#pragma once
+
+
+#include "../../../../../ConceptEngineFramework/Graphics/DirectX12/CEDX12Playground.h"
+#include "../../../../../ConceptEngineFramework/Graphics/DirectX12/Resources/CEFrameResource.h"
+#include "../../../../../ConceptEngineFramework/Graphics/DirectX12/Resources/CEWaves.h"
+
+using namespace ConceptEngineFramework::Graphics::DirectX12;
+using namespace ConceptEngineFramework::Game;
+
+namespace ConceptEngine::Playgrounds::DirectX12 {
+	class CEDX12TreesWavesPlayground final : public CEDX12Playground {
+	public:
+		CEDX12TreesWavesPlayground();
+
+		void Create() override;
+		void Update(const CETimer& gt) override;
+		void Render(const CETimer& gt) override;
+		void Resize() override;
+
+		void OnMouseDown(KeyCode key, int x, int y) override;
+		void OnMouseUp(KeyCode key, int x, int y) override;
+		void OnMouseMove(KeyCode key, int x, int y) override;
+		void OnKeyUp(KeyCode key, char keyChar, const CETimer& gt) override;
+		void OnKeyDown(KeyCode key, char keyChar, const CETimer& gt) override;
+		void OnMouseWheel(KeyCode key, float wheelDelta, int x, int y) override;
+
+		void UpdateCamera(const CETimer& gt) override;
+		void AnimateMaterials(const CETimer& gt) override;
+		void UpdateObjectCBs(const CETimer& gt) override;
+		void UpdateMaterialCBs(const CETimer& gt) override;
+		void UpdateMainPassCB(const CETimer& gt) override;
+
+	private:
+		void LoadTextures();
+		void BuildDescriptorHeaps();
+		void BuildLandGeometry();
+		void BuildWavesGeometryBuffers();
+		void BuildBoxGeometry();
+		void BuildMaterials();
+		void BuildRenderItems();
+		void BuildFrameResources();
+
+		void DrawRenderItems(ID3D12GraphicsCommandList* cmdList,
+			std::vector<Resources::RenderItem*>& ritems) const;
+
+		float GetHillsHeight(float x, float z) const;
+		XMFLOAT3 GetHillsNormal(float x, float z) const;
+
+		void UpdateWaves(const CETimer& gt);
+
+		std::vector<std::unique_ptr<Resources::CEFrameResource>> mFrameResources;
+
+		Resources::CEFrameResource* mCurrFrameResource = nullptr;
+		int mCurrFrameResourceIndex = 0;
+
+		std::unordered_map<std::string, std::unique_ptr<Resources::MeshGeometry>> mGeometries;
+		std::unordered_map<std::string, std::unique_ptr<Resources::Material>> mMaterials;
+		std::unordered_map<std::string, std::shared_ptr<Resources::Texture>> mTextures;
+
+		std::unordered_map<std::string, Microsoft::WRL::ComPtr<ID3DBlob>> mShaders;
+
+		Microsoft::WRL::ComPtr<ID3D12RootSignature> m_rootSignature;
+		Microsoft::WRL::ComPtr<ID3D12PipelineState> mOpaquePSO = nullptr;
+
+		Resources::RenderItem* mWavesRitem = nullptr;
+
+		//list of all render items.
+		std::vector<std::unique_ptr<Resources::RenderItem>> mAllRitems;
+
+		//Render items divided by PSO;
+		std::vector<Resources::RenderItem*> mOpaqueRitems;
+
+		Resources::PassConstants mMainPassCB;
+
+		std::unique_ptr<Resources::CEWaves> m_waves;
+
+		XMFLOAT3 mEyePos = { 0.0f, 0.0f, 0.0f };
+		float mSunTheta = 1.25f * XM_PI;
+		float mSunPhi = XM_PIDIV4;
+	};
+}
