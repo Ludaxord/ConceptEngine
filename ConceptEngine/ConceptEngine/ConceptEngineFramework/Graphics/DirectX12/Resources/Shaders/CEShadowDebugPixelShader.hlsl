5879ed2881a6a114fdb2ae89ca3879a48ac32e37
Konrad Uciechowski <konrad.uciechowski@gmail.com>
2021-03-27 20:35:53 +0100

    for now change debug pixel shader for ssao for shadow pixel shader

--- ConceptEngine/ConceptEngine/ConceptEngineFramework/Graphics/DirectX12/Resources/Shaders/CEShadowDebugPixelShader.hlsl
+++ ConceptEngine/ConceptEngine/ConceptEngineFramework/Graphics/DirectX12/Resources/Shaders/CEShadowDebugPixelShader.hlsl
@@ -14,7 +14,8 @@ float4 PS(VertexOut pin) : SV_Target{
 #if defined(SHADOW)
 	return float4(gShadowMap.Sample(gsamLinearWrap, pin.TexC).rrr, 1.0f);
 #elif defined(SSAO)
-	return float4(gSsaoMap.Sample(gsamLinearWrap, pin.TexC).rrr, 1.0f);
+	return float4(gShadowMap.Sample(gsamLinearWrap, pin.TexC).rrr, 1.0f);
+	// return float4(gSsaoMap.Sample(gsamLinearWrap, pin.TexC).rrr, 1.0f);
 #else
 	return float4(0.0f, 0.0f, 0.0f, 0.0f);
 #endif
