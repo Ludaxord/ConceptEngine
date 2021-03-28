ed9b79f111711298e062d5089c76bd08ef2c906b
Konrad Uciechowski <konrad.uciechowski@gmail.com>
2021-03-28 17:35:57 +0200

    prepare header for Loading model with movement

--- ConceptEngine/ConceptEngine/ConceptEngineFramework/Graphics/DirectX12/Resources/CEModelLoader.cpp
+++ ConceptEngine/ConceptEngine/ConceptEngineFramework/Graphics/DirectX12/Resources/CEModelLoader.cpp
@@ -1,1 +1,52 @@
 #include "CEModelLoader.h"
+
+#include <fstream>
+
+using namespace ConceptEngineFramework::Graphics::DirectX12::Resources;
+
+bool CEModelLoader::LoadM3D(const std::string& fileName, std::vector<Vertex>& vertices, std::vector<USHORT>& indices,
+                            std::vector<Subset> subsets, std::vector<Material>& materials) {
+    std::ifstream fin(fileName);
+
+    UINT numMaterials = 0;
+    UINT numVertices = 0;
+    UINT numTriangles = 0;
+    UINT numElements = 0;
+    UINT numAnimationClips = 0;
+}
+
+bool CEModelLoader::LoadM3D(const std::string& fileName, std::vector<ModelVertex>& vertices,
+                            std::vector<USHORT>& indices, std::vector<Subset>& subsets,
+                            std::vector<Material>& materials,
+                            CEModelData& modelInfo) {
+}
+
+void CEModelLoader::ReadMaterials(std::ifstream& fin, UINT numMaterials, std::vector<Material>& materials) {
+}
+
+void CEModelLoader::ReadSubsetTable(std::ifstream& fin, UINT numSubsets, std::vector<Subset>& subsets) {
+}
+
+void CEModelLoader::ReadVertices(std::ifstream& fin, UINT numVertices, std::vector<Vertex>& vertices) {
+}
+
+void CEModelLoader::ReadModelVertices(std::ifstream& fin, UINT numVertices, std::vector<ModelVertex>& vertices) {
+}
+
+void CEModelLoader::ReadTriangles(std::ifstream& fin, UINT numTriangles, std::vector<USHORT>& indices) {
+}
+
+void CEModelLoader::ReadElementOffsets(std::ifstream& fin, UINT numElements,
+                                       std::vector<DirectX::XMFLOAT4X4>& elementOffsets) {
+}
+
+void CEModelLoader::ReadElementHierarchy(std::ifstream& fin, UINT numElements,
+                                         std::vector<int>& elementIndexToParentIndex) {
+}
+
+void CEModelLoader::ReadAnimationClips(std::ifstream& fin, UINT numElements, UINT numAnimationClips,
+                                       std::unordered_map<std::string, CEAnimationClip>& animations) {
+}
+
+void CEModelLoader::ReadElementKeyFrames(std::ifstream& fin, UINT numElements, CEAnimation& animation) {
+}
--- ConceptEngine/ConceptEngine/ConceptEngineFramework/Graphics/DirectX12/Resources/CEModelLoader.h
+++ ConceptEngine/ConceptEngine/ConceptEngineFramework/Graphics/DirectX12/Resources/CEModelLoader.h
@@ -1,4 +1,10 @@
 #pragma once
+#include <DirectXMath.h>
+#include <intsafe.h>
+#include <string>
+#include <vector>
+
+#include "CEModel.h"
 
 namespace ConceptEngineFramework::Graphics::DirectX12::Resources {
 	enum class ModelExtension {
@@ -13,6 +19,68 @@ namespace ConceptEngineFramework::Graphics::DirectX12::Resources {
 	};
 
 	class CEModelLoader {
+	public:
+		struct Vertex {
+			DirectX::XMFLOAT3 Pos;
+			DirectX::XMFLOAT3 Normal;
+			DirectX::XMFLOAT2 TexC;
+			DirectX::XMFLOAT4 TangentU;
+		};
 
+		struct ModelVertex {
+			DirectX::XMFLOAT3 Pos;
+			DirectX::XMFLOAT3 Normal;
+			DirectX::XMFLOAT2 TexC;
+			DirectX::XMFLOAT4 TangentU;
+			DirectX::XMFLOAT3 ElementWeights;
+			BYTE ElementIndices[4];
+		};
+
+		struct Subset {
+			UINT Id = -1;
+			UINT VertexStart = 0;
+			UINT VertexCount = 0;
+			UINT FaceStart = 0;
+			UINT FaceCount = 0;
+		};
+
+		struct Material {
+			std::string Name;
+
+			DirectX::XMFLOAT4 DiffuseAlbedo = {};
+			DirectX::XMFLOAT4 FresnelR0 = {};
+			float Roughness = 0.8f;
+			bool AlphaClip = false;
+
+			std::string MaterialTypeName;
+			std::string DiffuseMapName;
+			std::string NormalMapName;
+		};
+
+		bool LoadM3D(const std::string& fileName,
+		             std::vector<Vertex>& vertices,
+		             std::vector<USHORT>& indices,
+		             std::vector<Subset> subsets,
+		             std::vector<Material>& materials);
+		bool LoadM3D(const std::string& fileName,
+		             std::vector<ModelVertex>& vertices,
+		             std::vector<USHORT>& indices,
+		             std::vector<Subset>& subsets,
+		             std::vector<Material>& materials,
+		             CEModelData& modelInfo);
+	protected:
+	private:
+		void ReadMaterials(std::ifstream& fin, UINT numMaterials, std::vector<Material>& materials);
+		void ReadSubsetTable(std::ifstream& fin, UINT numSubsets, std::vector<Subset>& subsets);
+		void ReadVertices(std::ifstream& fin, UINT numVertices, std::vector<Vertex>& vertices);
+		void ReadModelVertices(std::ifstream& fin, UINT numVertices, std::vector<ModelVertex>& vertices);
+		void ReadTriangles(std::ifstream& fin, UINT numTriangles, std::vector<USHORT>& indices);
+		void ReadElementOffsets(std::ifstream& fin, UINT numElements, std::vector<DirectX::XMFLOAT4X4>& elementOffsets);
+		void ReadElementHierarchy(std::ifstream& fin, UINT numElements, std::vector<int>& elementIndexToParentIndex);
+		void ReadAnimationClips(std::ifstream& fin,
+		                        UINT numElements,
+		                        UINT numAnimationClips,
+		                        std::unordered_map<std::string, CEAnimationClip>& animations);
+		void ReadElementKeyFrames(std::ifstream& fin, UINT numElements, CEAnimation& animation);
 	};
 }
