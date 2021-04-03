91a0e67c9e533ca12bb8ccba29ab02630610a885
Konrad Uciechowski <konrad.uciechowski@gmail.com>
2021-04-03 17:12:47 +0200

    add comment with warn what need to be fixed

--- ConceptEngine/ConceptEngine/ConceptEngineFramework/Graphics/DirectX12/Resources/Shaders/CEShadowDebugPixelShader.hlsl
+++ ConceptEngine/ConceptEngine/ConceptEngineFramework/Graphics/DirectX12/Resources/Shaders/CEShadowDebugPixelShader.hlsl
@@ -14,6 +14,7 @@ float4 PS(VertexOut pin) : SV_Target{
 #if defined(SHADOW)
 	return float4(gShadowMap.Sample(gsamLinearWrap, pin.TexC).rrr, 1.0f);
 #elif defined(SSAO)
+	//TODO: fix shadowMap
 	return float4(gShadowMap.Sample(gsamLinearWrap, pin.TexC).rrr, 1.0f);
 	// return float4(gSsaoMap.Sample(gsamLinearWrap, pin.TexC).rrr, 1.0f);
 #else
