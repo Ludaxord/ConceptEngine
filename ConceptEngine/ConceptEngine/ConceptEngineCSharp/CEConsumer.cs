e76076e1cf89b14ebc404c98ae2f5090ce68b88d
Konrad Uciechowski <konrad.uciechowski@gmail.com>
2021-04-27 22:12:16 +0200

    create consumer to build all elements created from user

---
+++ ConceptEngine/ConceptEngine/ConceptEngineCSharp/CEConsumer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceptEngineCSharp
+{
+    public static class CEConsumer
+    {
+        //Debug
+        public static CECSPlayground GetPlayground()
+        {
+            return new CECSPlayground();
+        }
+
+        public static void GeneratePlayground()
+        {
+
+        }
+
+        public static void CompilePlayground()
+        {
+
+        }
+
+        public static void CleanPlayground()
+        {
+
+        }
+
+        public static void GenerateProject()
+        {
+
+        }
+
+        public static void CompileProject()
+        {
+
+        }
+
+        public static void CleanProject()
+        {
+
+        }
+    }
+}
