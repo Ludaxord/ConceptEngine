a5faacb66a0156150e99a29d11e1e6a6eaa82405
Konrad Uciechowski <konrad.uciechowski@gmail.com>
2021-05-04 00:37:22 +0200

    create render layer and input

---
+++ ConceptEngine/ConceptEngine/Concept-Engine/Core/Platform/Windows/Input/CEInputManager.cpp
@@ -0,0 +1,1 @@
+#include "CEInputManager.h"
---
+++ ConceptEngine/ConceptEngine/Concept-Engine/Core/Platform/Windows/Input/CEInputManager.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "../../../Common/CEManager.h"
+
+namespace ConceptEngine::Core::Platform::Windows::Input {
+	class CEInputManager : public Common::CEManager {
+
+	};
+}
