ee2108f82badb92c1e253bcf255467f0f8fe094f
Konrad Uciechowski <konrad.uciechowski@gmail.com>
2021-05-06 23:50:06 +0200

    fill iterator class

--- ConceptEngine/ConceptEngine/Concept-Engine/Core/Containers/CEIterator.h
+++ ConceptEngine/ConceptEngine/Concept-Engine/Core/Containers/CEIterator.h
@@ -2,8 +2,93 @@
 namespace ConceptEngine::Core::Containers {
 	template <typename TIteratorType>
 	class CEReverseIterator {
+
 	public:
-	protected:
+		CEReverseIterator() noexcept
+			: Iterator(nullptr) {
+		}
+
+		CEReverseIterator(TIteratorType* InIterator) noexcept
+			: Iterator(InIterator) {
+		}
+
+		CEReverseIterator(const CEReverseIterator& Other) noexcept
+			: Iterator(Other.Raw()) {
+		}
+
+		template <typename U>
+		CEReverseIterator(const CEReverseIterator<U>& Other) noexcept
+			: Iterator(Other.Raw()) {
+		}
+
+		~CEReverseIterator() = default;
+
+		TIteratorType* Raw() const {
+			return Iterator;
+		}
+
+		TIteratorType* operator->() const noexcept {
+			return (Iterator - 1);
+		}
+
+		TIteratorType& operator*() const noexcept {
+			return *(Iterator - 1);
+		}
+
+		CEReverseIterator operator++() noexcept {
+			Iterator--;
+			return *this;
+		}
+
+		CEReverseIterator operator++(int32) noexcept {
+			CEReverseIterator Temp = *this;
+			Iterator--;
+			return Temp;
+		}
+
+		CEReverseIterator operator--() noexcept {
+			Iterator++;
+			return *this;
+		}
+
+		CEReverseIterator operator--(int32) noexcept {
+			CEReverseIterator Temp = *this;
+			Iterator++;
+			return Temp;
+		}
+
+		CEReverseIterator operator+(int32 Offset) const noexcept {
+			CEReverseIterator Temp = *this;
+			return Temp += Offset;
+		}
+
+		CEReverseIterator operator-(int32 Offset) const noexcept {
+			CEReverseIterator Temp = *this;
+			return Temp -= Offset;
+		}
+
+		CEReverseIterator& operator+=(int32 Offset) noexcept {
+			Iterator -= Offset;
+			return *this;
+		}
+
+		CEReverseIterator& operator-=(int32 Offset) noexcept {
+			Iterator += Offset;
+			return *this;
+		}
+
+		bool operator==(const CEReverseIterator& Other) const noexcept {
+			return (Iterator == Other.Iterator);
+		}
+
+		bool operator!=(const CEReverseIterator& Other) const noexcept {
+			return (Iterator != Other.Iterator);
+		}
+
+		operator CEReverseIterator<const TIteratorType>() const noexcept {
+			return CEReverseIterator<const TIteratorType>(Iterator);
+		}
+
 	private:
 		TIteratorType* Iterator;
 	};
