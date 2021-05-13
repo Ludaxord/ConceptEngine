9f6ad85a309f50e2b4bffdfce2e5b04b8d799689
Konrad Uciechowski <konrad.uciechowski@gmail.com>
2021-05-14 00:31:18 +0200

    save project

--- ConceptEngine/ConceptEngine/Concept-Engine/Graphics/Main/RenderLayer/CEICommandContext.h
+++ ConceptEngine/ConceptEngine/Concept-Engine/Graphics/Main/RenderLayer/CEICommandContext.h
@@ -8,5 +8,14 @@ namespace ConceptEngine::Graphics::Main::RenderLayer {
 		virtual void Begin() = 0;
 		virtual void End() = 0;
 		virtual void BeginTimeStamp() = 0;
+		virtual void EndTimeStamp() = 0;
+		virtual void ClearRenderTargetView() = 0;
+		virtual void ClearDepthStencilView() = 0;
+		virtual void ClearUnorderedAccessViewFloat() = 0;
+		virtual void SetShadingRate() = 0;
+		virtual void SetShadingRateImage() = 0;
+		virtual void SetViewport() = 0;
+		virtual void SetScissorRect() = 0;
+		virtual void SetBlendFactor() = 0;
 	};
 }
