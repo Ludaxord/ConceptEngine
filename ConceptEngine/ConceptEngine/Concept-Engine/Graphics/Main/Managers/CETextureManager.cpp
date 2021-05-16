6186501acae420bf70f5c3ea16ae4a923da9cd26
Konrad Uciechowski <konrad.uciechowski@gmail.com>
2021-05-16 22:02:25 +0200

    create texture manager basic header definitions

--- ConceptEngine/ConceptEngine/Concept-Engine/Graphics/Main/Managers/CETextureManager.cpp
+++ ConceptEngine/ConceptEngine/Concept-Engine/Graphics/Main/Managers/CETextureManager.cpp
@@ -1,11 +1,29 @@
 #include "CETextureManager.h"
 
-ConceptEngine::Graphics::Main::Managers::CETextureManager::CETextureManager(): CEManager() {
+using namespace ConceptEngine::Graphics::Main::Managers;
+using namespace ConceptEngine::Graphics::Main::RenderLayer;
+
+CETextureManager::CETextureManager(): CEManager() {
 }
 
-ConceptEngine::Graphics::Main::Managers::CETextureManager::~CETextureManager() {
+CETextureManager::~CETextureManager() {
 }
 
-bool ConceptEngine::Graphics::Main::Managers::CETextureManager::Create() {
+bool CETextureManager::Create() {
 	return true;
+}
+
+CETexture2D* CETextureManager::LoadFromFile(const std::string& filePath, uint32 flags, RenderLayer::CEFormat format) {
+	return nullptr;
+}
+
+CETexture2D* CETextureManager::LoadFromMemory(const uint8* pixels, uint32 width, uint32 height, uint32 createFlags,
+                                              CEFormat format) {
+	return nullptr;
+}
+
+CETextureCube* CETextureManager::CreateTextureCubeFromPanorama(CETexture2D* panoramaSource, uint32 cubeMapSize,
+                                                               uint32 flags,
+                                                               CEFormat format) {
+	return nullptr;
 }
--- ConceptEngine/ConceptEngine/Concept-Engine/Graphics/Main/Managers/CETextureManager.h
+++ ConceptEngine/ConceptEngine/Concept-Engine/Graphics/Main/Managers/CETextureManager.h
@@ -1,12 +1,32 @@
 #pragma once
 
+#include <string>
+
 #include "../../../Core/Common/CEManager.h"
+#include "../../../Core/Common/CETypes.h"
+#include "../RenderLayer/CETexture.h"
+
 
 namespace ConceptEngine::Graphics::Main::Managers {
+	enum CETextureFlags : uint32 {
+		TextureFlag_None = 0,
+		TextureFlag_GenerateMips = FLAG(1)
+	};
+
 	class CETextureManager : public Core::Common::CEManager {
 	public:
 		CETextureManager();
 		~CETextureManager() override;
+		
 		virtual bool Create() override;
+
+		static class RenderLayer::CETexture2D* LoadFromFile(const std::string& filePath, uint32 flags,
+		                                                    RenderLayer::CEFormat format);
+		static class RenderLayer::CETexture2D* LoadFromMemory(const uint8* pixels, uint32 width, uint32 height,
+		                                                      uint32 createFlags, RenderLayer::CEFormat format);
+
+		static class RenderLayer::CETextureCube* CreateTextureCubeFromPanorama(
+			class RenderLayer::CETexture2D* panoramaSource, uint32 cubeMapSize, uint32 flags,
+			RenderLayer::CEFormat format);
 	};
 }
