4fee1486802f327eaa6a99f4019d70e8951a2634
Konrad Uciechowski <konrad.uciechowski@gmail.com>
2021-05-25 00:39:08 +0200

    create command list mapping for base functions of dx12 command list

--- ConceptEngine/ConceptEngine/Concept-Engine/Graphics/DirectX12/RenderLayer/CEDXCommandList.h
+++ ConceptEngine/ConceptEngine/Concept-Engine/Graphics/DirectX12/RenderLayer/CEDXCommandList.h
@@ -6,6 +6,7 @@
 #include "CEDXDevice.h"
 #include "CEDXDeviceElement.h"
 #include "CEDXResource.h"
+#include "CEDXRootSignature.h"
 #include "CEDXViews.h"
 #include "../../../Core/Common/CETypes.h"
 
@@ -60,10 +61,6 @@ namespace ConceptEngine::Graphics::DirectX12::RenderLayer {
 			                                           clearColor, 0, nullptr);
 		}
 
-		void ResourceBarrier(struct D3D12_RESOURCE_BARRIER* barriers, uint32 numBarriers) {
-			CommandList->ResourceBarrier(numBarriers, barriers);
-		}
-
 		void CopyBufferRegion(CEDXResource* destination, uint64 destinationOffset, CEDXResource* source,
 		                      uint64 sourceOffset, uint64 sizeInBytes) {
 			CopyBufferRegion(destination->GetResource(), destinationOffset, source->GetResource(), sourceOffset,
@@ -75,12 +72,194 @@ namespace ConceptEngine::Graphics::DirectX12::RenderLayer {
 			CommandList->CopyBufferRegion(destination, destinationOffset, source, sourceOffset, sizeInBytes);
 		};
 
-		//TODO: Finish mapping all functions of Command List
-	
+		void CopyTextureRegion(const D3D12_TEXTURE_COPY_LOCATION* destination, uint32 x, uint32 y, uint32 z,
+		                       const D3D12_TEXTURE_COPY_LOCATION* source, const D3D12_BOX* sourceBox) {
+			CommandList->CopyTextureRegion(destination, x, y, z, source, sourceBox);
+		}
+
+		void CopyResource(CEDXResource* destination, CEDXResource* source) {
+			CopyResource(destination->GetResource(), source->GetResource());
+		}
+
+		void CopyResource(ID3D12Resource* destination, ID3D12Resource* source) {
+			CommandList->CopyResource(destination, source);
+		}
+
+		void ResolveSubresource(CEDXResource* destination, CEDXResource* source, DXGI_FORMAT format) {
+			CommandList->ResolveSubresource(destination->GetResource(), 0, source->GetResource(), 0, format);
+		}
+
+		void BuildRayTracingAccelerationStructure(const D3D12_BUILD_RAYTRACING_ACCELERATION_STRUCTURE_DESC* desc) {
+			CommandList5->BuildRaytracingAccelerationStructure(desc, 0, nullptr);
+		}
+
+		void DispatchRays(const D3D12_DISPATCH_RAYS_DESC* desc) {
+			CommandList5->DispatchRays(desc);
+		}
+
+		void Dispatch(uint32 threadGroupCountX, uint32 threadGroupCountY, uint32 threadGroupCountZ) {
+			CommandList->Dispatch(threadGroupCountX, threadGroupCountY, threadGroupCountZ);
+		}
+
+		void DrawInstanced(uint32 vertexCountPerInstance,
+		                   uint32 instanceCount,
+		                   uint32 startVertexLocation,
+		                   uint32 startInstanceLocation) {
+			CommandList->DrawInstanced(vertexCountPerInstance,
+			                           instanceCount,
+			                           startVertexLocation,
+			                           startInstanceLocation);
+		}
+
+		void DrawIndexedInstanced(uint32 indexCountPerInstance,
+		                          uint32 instanceCount,
+		                          uint32 startIndexLocation,
+		                          uint32 baseVertexLocation,
+		                          uint32 startInstanceLocation) {
+			CommandList->DrawIndexedInstanced(indexCountPerInstance,
+			                                  instanceCount,
+			                                  startIndexLocation,
+			                                  baseVertexLocation,
+			                                  startInstanceLocation);
+		}
+
+		void SetDescriptorHeaps(ID3D12DescriptorHeap* const* descriptorHeaps, uint32 descriptorHeapCount) {
+			CommandList->SetDescriptorHeaps(descriptorHeapCount, descriptorHeaps);
+		}
+
+		void SetStateObject(ID3D12StateObject* stateObject) {
+			CommandList5->SetPipelineState1(stateObject);
+		}
+
+		void SetPipelineState(ID3D12PipelineState* pipelineState) {
+			CommandList->SetPipelineState(pipelineState);
+		}
+
+		void SetComputeRootSignature(CEDXRootSignature* rootSignature) {
+			CommandList->SetComputeRootSignature(rootSignature->GetRootSignature());
+		}
+
+		void SetGraphicsRootSignature(CEDXRootSignature* rootSignature) {
+			CommandList->SetGraphicsRootSignature(rootSignature->GetRootSignature());
+		}
+
+		void SetComputeRootDescriptorTable(uint32 rootParameterIndex, D3D12_GPU_DESCRIPTOR_HANDLE baseDescriptor) {
+			CommandList->SetComputeRootDescriptorTable(rootParameterIndex, baseDescriptor);
+		}
+
+		void SetGraphicsRootDescriptorTable(uint32 rootParameterIndex, D3D12_GPU_DESCRIPTOR_HANDLE baseDescriptor) {
+			CommandList->SetGraphicsRootDescriptorTable(rootParameterIndex, baseDescriptor);
+		}
+
+		void SetComputeRoot32BitConstants(const void* sourceData,
+		                                  uint32 num32BitValues,
+		                                  uint32 destOffsetIn32BitValues,
+		                                  uint32 rootParameterIndex) {
+			CommandList->SetComputeRoot32BitConstants(rootParameterIndex, num32BitValues, sourceData,
+			                                          destOffsetIn32BitValues);
+		}
+
+		void SetGraphicsRoot32BitConstants(const void* sourceData,
+		                                   uint32 num32BitValues,
+		                                   uint32 destOffsetIn32BitValues,
+		                                   uint32 rootParameterIndex) {
+			CommandList->SetGraphicsRoot32BitConstants(rootParameterIndex, num32BitValues, sourceData,
+			                                           destOffsetIn32BitValues);
+		}
+
+		void SetVertexBuffers(uint32 start, const D3D12_VERTEX_BUFFER_VIEW* vertexBufferViews,
+		                      uint32 vertexBufferViewCount) {
+			CommandList->IASetVertexBuffers(start, vertexBufferViewCount, vertexBufferViews);
+		}
+
+		void SetIndexBuffer(const D3D12_INDEX_BUFFER_VIEW* indexBufferView) {
+			CommandList->IASetIndexBuffer(indexBufferView);
+		}
+
+		void SetPrimitiveTopology(D3D12_PRIMITIVE_TOPOLOGY primitiveTopology) {
+			CommandList->IASetPrimitiveTopology(primitiveTopology);
+		}
+
+		void SetViewports(const D3D12_VIEWPORT* viewports, uint32 viewportCount) {
+			CommandList->RSSetViewports(viewportCount, viewports);
+		}
+
+		void SetScissorRects(const D3D12_RECT* scissorRects, uint32 scissorRectCount) {
+			CommandList->RSSetScissorRects(scissorRectCount, scissorRects);
+		}
+
+		void SetShadingRate(D3D12_SHADING_RATE baseShadingRate, const D3D12_SHADING_RATE_COMBINER* combiners) {
+			CommandList5->RSSetShadingRate(baseShadingRate, combiners);
+		}
+
+		void SetShadingRateImage(ID3D12Resource* shadingRateImage) {
+			CommandList5->RSSetShadingRateImage(shadingRateImage);
+		}
+
+		void SetBlendFactor(const float blendFactor[4]) {
+			CommandList->OMSetBlendFactor(blendFactor);
+		}
+
+		void SetRenderTargets(const D3D12_CPU_DESCRIPTOR_HANDLE* renderTargetDescriptors,
+		                      uint32 numRenderTargetsDescriptors, bool rtSingleHandletoDescriptorRange,
+		                      const D3D12_CPU_DESCRIPTOR_HANDLE* depthStencilDescriptor) {
+			CommandList->OMSetRenderTargets(numRenderTargetsDescriptors, renderTargetDescriptors,
+			                                rtSingleHandletoDescriptorRange, depthStencilDescriptor);
+		}
+
+		void ResourceBarrier(const D3D12_RESOURCE_BARRIER* barriers, uint32 numBarriers) {
+			CommandList->ResourceBarrier(numBarriers, barriers);
+		}
+
+		void TransitionBarrier(ID3D12Resource* resource, D3D12_RESOURCE_STATES beforeState,
+		                       D3D12_RESOURCE_STATES afterState, UINT subResource) {
+			D3D12_RESOURCE_BARRIER barrier;
+			Memory::CEMemory::Memzero(&barrier);
+
+			barrier.Type = D3D12_RESOURCE_BARRIER_TYPE_TRANSITION;
+			barrier.Transition.pResource = resource;
+			barrier.Transition.StateAfter = afterState;
+			barrier.Transition.StateBefore = beforeState;
+			barrier.Transition.Subresource = subResource;
+
+			CommandList->ResourceBarrier(1, &barrier);
+		}
+
+		void UnorderedAccessBarrier(ID3D12Resource* resource) {
+			D3D12_RESOURCE_BARRIER barrier;
+			Memory::CEMemory::Memzero(&barrier);
+
+			barrier.Type = D3D12_RESOURCE_BARRIER_TYPE_UAV;
+			barrier.UAV.pResource = resource;
+
+			CommandList->ResourceBarrier(1, &barrier);
+		}
+
+		bool IsRecording() const {
+			return IsReady;
+		}
+
+		void SetName(const std::string& name) {
+			std::wstring wName = ConvertToWString(name);
+			CommandList->SetName(wName.c_str());
+		}
+
+		ID3D12CommandList* GetCommandList() const {
+			return CommandList.Get();
+		}
+
+		ID3D12GraphicsCommandList* GetGraphicsCommandList() const {
+			return CommandList.Get();
+		}
+
+		ID3D12GraphicsCommandList5* GetDXRCommandList() const {
+			return CommandList5.Get();
+		}
+
 	protected:
 	private:
 		Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> CommandList;
-		Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> CommandList5;
+		Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList5> CommandList5;
 		bool IsReady = false;
 	};
 }
