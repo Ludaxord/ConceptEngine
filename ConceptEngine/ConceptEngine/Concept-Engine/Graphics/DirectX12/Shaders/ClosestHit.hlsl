874f46d8ee71c436adbb1f18b2d3ae21024018ba
Konrad Uciechowski <konrad.uciechowski@gmail.com>
2021-06-08 00:01:48 +0200

    create closest hit initializers from cpu

--- ConceptEngine/ConceptEngine/Concept-Engine/Graphics/DirectX12/Shaders/ClosestHit.hlsl
+++ ConceptEngine/ConceptEngine/Concept-Engine/Graphics/DirectX12/Shaders/ClosestHit.hlsl
@@ -1,3 +1,19 @@
+#include "PBRHelpers.hlsli"
+#include "Structs.hlsli"
+#include "RayTracingHelpers.hlsli"
+#include "Constants.hlsli"
+
+RaytracingAccelerationStructure Scene : register(t0, space0);
+
+ConstantBuffer<Camera> CameraBuffer : register(b0, space0);
+
+TextureCube<float4> Skybox : register(t1, space0);
+Texture2D<float4> MaterialTextures[128] : register(t4, space0);
+
+StructuredBuffer<Vertex> Vertices : register(t0, D3D12_SHADER_REGISTER_SPACE_RT_LOCAL);
+ByteAddressBuffer Indices : register(t1, D3D12_SHADER_REGISTER_SPACE_RT_LOCAL);
+
+[shader("closestHit")]
 void ClosestHit() {
-	
+
 }
--- ConceptEngine/ConceptEngine/Concept-Engine/Graphics/DirectX12/Shaders/Structs.hlsli
+++ ConceptEngine/ConceptEngine/Concept-Engine/Graphics/DirectX12/Shaders/Structs.hlsli
@@ -8,4 +8,25 @@ struct ComputeShaderInput {
 	uint GroupIndex : SV_GroupIndex;
 };
 
+struct Camera {
+	float4x4 ViewProjection;
+	float4x4 View;
+	float4x4 ViewInverse;
+	float4x4 Projection;
+	float4x4 ProjectionInverse;
+	float4x4 ViewProjectionInverse;
+	float3 Position;
+	float NearPlane;
+	float3 Forward;
+	float FarPlane;
+	float AspectRatio;
+};
+
+struct Vertex {
+	float3 Position;
+	float3 Normal;
+	float3 Tangent;
+	float2 TexCoord;
+};
+
 #endif
