bd11e1145934c7f47fa87a16b7f084f63e0a7feb
Konrad Uciechowski <konrad.uciechowski@gmail.com>
2021-07-12 22:24:55 +0200

    add implementation to scene functions

--- ConceptEngine/ConceptEngine/Concept-Engine/Render/Scene/CEScene.cpp
+++ ConceptEngine/ConceptEngine/Concept-Engine/Render/Scene/CEScene.cpp
@@ -1,1 +1,28 @@
 #include "CEScene.h"
+
+using namespace ConceptEngine::Render::Scene;
+
+CEScene::CEScene() {
+}
+
+CEScene::~CEScene() {
+}
+
+void CEScene::Update(Time::CETimestamp deltaTime) {
+}
+
+void CEScene::AddCamera(CECamera* camera) {
+}
+
+void CEScene::AddActor(CEActor* actor) {
+}
+
+void CEScene::OnAddedComponent(CEComponent* component) {
+}
+
+CEScene* CEScene::LoadFromFile(const std::string& filePath) {
+	return nullptr;
+}
+
+void CEScene::AddMeshComponent(CEMeshComponent* component) {
+}
