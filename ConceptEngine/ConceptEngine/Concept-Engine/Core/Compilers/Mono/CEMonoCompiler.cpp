1c500bb1beebecb117513a5cb852f6fa7e6b473e
Konrad Uciechowski <konrad.uciechowski@gmail.com>
2021-07-12 21:22:48 +0200

    create mono implementation

--- ConceptEngine/ConceptEngine/Concept-Engine/Core/Compilers/Mono/CEMonoCompiler.cpp
+++ ConceptEngine/ConceptEngine/Concept-Engine/Core/Compilers/Mono/CEMonoCompiler.cpp
@@ -1,12 +1,80 @@
 #include "CEMonoCompiler.h"
 
+#include <unordered_map>
+
+#include "../../Log/CELog.h"
+
 using namespace ConceptEngine::Core::Compilers::Mono;
 
-CEMonoCompiler::CEMonoCompiler() {
+CEMonoCompiler::CEMonoCompiler(CEArray<CEMonoData>& MonoData): Domain(nullptr) {
+	MonoArray = MonoData;
 }
 
-void CEMonoCompiler::Create() {
+CEMonoCompiler::CEMonoCompiler(CEMonoData& MonoData): Domain(nullptr) {
+	MonoArray.EmplaceBack(MonoData);
+}
+
+CEMonoCompiler::CEMonoCompiler(std::string& DllPath,
+                               std::string& FilePath): Domain(nullptr) {
+	MonoArray.EmplaceBack(CEMonoData(DllPath, FilePath));
+}
+
+CEMonoCompiler::CEMonoCompiler(std::unordered_map<std::string&, std::string&> FilePaths): Domain(nullptr) {
+	for (auto& uMap : FilePaths) {
+		MonoArray.EmplaceBack(CEMonoData(uMap.first, uMap.second));
+	}
+}
+
+bool CEMonoCompiler::Create() {
 	//TODO: During installation process grab information from system environment (or install mono if is not available at user system). For now just pass Mono path.
 	mono_set_dirs("C:\\Program Files\\Mono\\lib", "C:\\Program Files\\Mono\\etc");
 
+	Domain = mono_jit_init("MonoScriptTry");
+	if (!Domain) {
+		CE_LOG_ERROR("[CEMonoCompiler] Failed to create Mono Domain")
+		return false;
+	}
+
+	for (auto& MonoData : MonoArray) {
+		MonoData.Assembly = CreateAssembly(MonoData.DllFilePath.c_str());
+		if (!MonoData.Assembly) {
+			CE_LOG_ERROR("[CEMonoCompiler] Failed to create Assembly for DllFile: " + MonoData.DllFilePath)
+			return false;
+		}
+
+		MonoData.Image = CreateImage(MonoData.Assembly);
+		if (!MonoData.Image) {
+			CE_LOG_ERROR("[CEMonoCompiler] Failed to create Image for DllFile: " + MonoData.DllFilePath)
+			return false;
+		}
+	}
+
+	return true;
+}
+
+bool CEMonoCompiler::AddFilePath(std::pair<std::string&, std::string&> FilePath) {
+	CEMonoData MonoData = CEMonoData(FilePath.first, FilePath.second);
+	MonoData.Assembly = CreateAssembly(MonoData.DllFilePath.c_str());
+	if (!MonoData.Assembly) {
+		CE_LOG_ERROR("[CEMonoCompiler] Failed to create Assembly for DllFile: " + MonoData.DllFilePath)
+		return false;
+	}
+
+	MonoData.Image = CreateImage(MonoData.Assembly);
+	if (!MonoData.Image) {
+		CE_LOG_ERROR("[CEMonoCompiler] Failed to create Image for DllFile: " + MonoData.DllFilePath)
+		return false;
+	}
+
+	MonoArray.EmplaceBack(MonoData);
+
+	return true;
+}
+
+MonoAssembly* CEMonoCompiler::CreateAssembly(const char* DllFilePath) const {
+	return mono_domain_assembly_open(Domain, DllFilePath);
+}
+
+MonoImage* CEMonoCompiler::CreateImage(MonoAssembly* Assembly) const {
+	return mono_assembly_get_image(Assembly);
 }
--- ConceptEngine/ConceptEngine/Concept-Engine/Core/Compilers/Mono/CEMonoCompiler.h
+++ ConceptEngine/ConceptEngine/Concept-Engine/Core/Compilers/Mono/CEMonoCompiler.h
@@ -1,21 +1,59 @@
 #pragma once
 
+#include <string>
+#include <unordered_map>
+
 #include "../CECompiler.h"
+#include "../../Containers/CEArray.h"
 
 #include <mono/jit/jit.h>
 #include <mono/metadata/assembly.h>
 #include <mono/metadata/debug-helpers.h>
 
 namespace ConceptEngine::Core::Compilers::Mono {
+	struct CEMonoData {
+		CEMonoData() = default;
+
+		CEMonoData(MonoAssembly* InAssembly, MonoImage* InImage): Assembly(InAssembly), Image(InImage) {
+
+		}
+
+		CEMonoData(MonoAssembly* InAssembly,
+		           MonoImage* InImage,
+		           std::string& InDllFilePath,
+		           std::string& InCSFilePath): CEMonoData(InAssembly, InImage) {
+			DllFilePath = InDllFilePath;
+			CSFilePath = InCSFilePath;
+		}
+
+		CEMonoData(std::string& InDllFilePath, std::string& InCSFilePath) {
+			DllFilePath = InDllFilePath;
+			CSFilePath = InCSFilePath;
+		}
+
+		MonoAssembly* Assembly = nullptr;
+		MonoImage* Image = nullptr;
+		std::string DllFilePath;
+		std::string CSFilePath;
+	};
+
 	class CEMonoCompiler : public CECompiler {
 	public:
-		CEMonoCompiler();
+		CEMonoCompiler(CEArray<CEMonoData>& MonoData);
+		CEMonoCompiler(CEMonoData& MonoData);
+		CEMonoCompiler(std::string& DllPath, std::string& FilePath);
+		CEMonoCompiler(std::unordered_map<std::string&, std::string&> FilePaths);
 
-	void Create();
-	protected:
+		bool Create();
+
+		void AddFilePath(std::pair<std::string&, std::string&> FilePath);
+
+	private:
+		MonoAssembly* CreateAssembly(const char* DllFilePath) const;
+		MonoImage* CreateImage(MonoAssembly* Assembly) const;
+
 	private:
 		MonoDomain* Domain;
-		MonoAssembly* Assembly;
-		MonoImage* Image;
+		CEArray<CEMonoData> MonoArray;
 	};
 }
