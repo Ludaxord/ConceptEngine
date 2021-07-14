9c96c43722abb19b2ae73dd0b013d65ad5d1b89b
Konrad Uciechowski <konrad.uciechowski@gmail.com>
2021-07-14 20:57:55 +0200

    add implementation of mesh class

--- ConceptEngine/ConceptEngine/Concept-Engine/Graphics/Main/Common/CEMesh.cpp
+++ ConceptEngine/ConceptEngine/Concept-Engine/Graphics/Main/Common/CEMesh.cpp
@@ -1,3 +1,15 @@
 #include "CEMesh.h"
 
 using namespace ConceptEngine::Graphics::Main::Common;
+
+bool CEMesh::Create(const CEMeshData& data) {
+}
+
+bool CEMesh::BuildAccelerationStructure(ConceptEngine::Graphics::Main::RenderLayer::CECommandList& commandList) {
+}
+
+CESharedPtr<CEMesh> CEMesh::Make(const CEMeshData& data) {
+}
+
+void CEMesh::CreateBoundingBox(const CEMeshData& data) {
+}
