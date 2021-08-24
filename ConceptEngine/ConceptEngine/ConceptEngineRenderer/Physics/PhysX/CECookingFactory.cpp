ae4e9e8cff489e52f46c6f1fbb9416a66bfaddb9
Konrad Uciechowski <konrad.uciechowski@gmail.com>
2021-08-24 23:31:05 +0200

    initialize create function in cooking factory

--- ConceptEngine/ConceptEngine/ConceptEngineRenderer/Physics/PhysX/CECookingFactory.cpp
+++ ConceptEngine/ConceptEngine/ConceptEngineRenderer/Physics/PhysX/CECookingFactory.cpp
@@ -1,8 +1,34 @@
 #include "CECookingFactory.h"
 
+#include <PxPhysics.h>
+#include <PxPhysicsVersion.h>
+#include <cooking/PxCooking.h>
+
+#include "CEPhysX.h"
+
+struct CECookingData {
+	CECookingData(const physx::PxTolerancesScale& Scale) : CookingSDK(nullptr), CookingParameters(Scale) {
+
+	}
+
+	physx::PxCooking* CookingSDK;
+	physx::PxCookingParams CookingParameters;
+};
+
+inline static CECookingData* CookingData = nullptr;
+
 //TODO: Implement
 bool CECookingFactory::Create() {
-	return false;
+	CookingData = new CECookingData(CEPhysX::GetPhysXSDK().getTolerancesScale());
+	CookingData->CookingParameters.meshWeldTolerance = 0.1f;
+	CookingData->CookingParameters.meshPreprocessParams = physx::PxMeshPreprocessingFlag::eWELD_VERTICES;
+	CookingData->CookingParameters.midphaseDesc = physx::PxMeshMidPhase::eBVH34;
+
+	CookingData->CookingSDK = PxCreateCooking(PX_PHYSICS_VERSION, CEPhysX::GetFoundation(),
+	                                          CookingData->CookingParameters);
+	Assert(CookingData->CookingSDK);
+
+	return true;
 }
 
 //TODO: Implement
@@ -27,4 +53,8 @@ void CECookingFactory::GenerateDebugMesh(CEColliderMeshComponent& Component, con
 
 //TODO: Implement
 void CECookingFactory::Release() {
+	CookingData->CookingSDK->release();
+	CookingData->CookingSDK = nullptr;
+
+	delete CookingData;
 }
