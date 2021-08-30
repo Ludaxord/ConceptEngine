a7ee5e1aa584bbdd2d6c3ce8db62800b3964bacc
Konrad Uciechowski <konrad.uciechowski@gmail.com>
2021-08-30 20:56:56 +0200

    rename CSharp Compiler to Mono Compiler

--- ConceptEngine/ConceptEngine/ConceptEngineRenderer/Compiler/CECppCompiler.h
+++ ConceptEngine/ConceptEngine/ConceptEngineRenderer/Compiler/CECppCompiler.h
@@ -2,5 +2,27 @@
 #include "CECompiler.h"
 
 class CECppCompiler : public CECompiler {
-	
+public:
+	void Create(const std::string& SourcePath) override;
+	void Release() override;
+	void OnSceneDestruct(CEUUID SceneID) override;
+	bool LoadRuntimeScript(const std::string& ScriptPath) override;
+	bool LoadAppScript(const std::string& ScriptPath) override;
+	bool ReloadScript(const std::string& ScriptPath) override;
+	void SetSceneContext(const CEScene* Scene) override;
+	const CEScene* GetCurrentSceneContext() override;
+	void OnCreateActor(Actor* InActor) override;
+	void OnUpdateActor(Actor* InActor, CETimestamp Timestamp) override;
+	void OnPhysicsUpdate(Actor* InActor) override;
+	void OnCollisionBegin(Actor* InActor) override;
+	void OnCollisionEnd(Actor* InActor) override;
+	void OnTriggerBegin(Actor* InActor) override;
+	void OnTriggerEnd(Actor* InActor) override;
+	bool IsActorModuleValid(Actor* Actor) override;
+	void OnScriptComponentDestroyed(CEUUID SceneID, CEUUID ActorID) override;
+	bool ModuleExists(const std::string& ModuleName) override;
+	void CreateActorScript(Actor* InActor) override;
+	void ReleaseActorScript(Actor* InActor, const std::string& ModuleName) override;
+	void InstanceActorClass(Actor* InActor) override;
+	void OnEditorRender() override;
 };
